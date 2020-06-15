8f9e0dc2fbea69a8515f6041366cee080f46fa53
iswoad <iswoadansary@gmail.com>
2020-06-16 00:45:42 +0600

    self added

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Card = ({name, email, id}) => {
+	return(
+		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+
+			<img alt='robot' 
+			src={`https://robohash.org/${id}$?200x200`}/>
+			<h1>{ name }</h1>
+			<p>{ email }</p>
+
+		</div>
+	);
+}
+
+export default Card;
