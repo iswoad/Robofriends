71357f24e744994db67f7130288201488aaadb42
iswoad <iswoadansary@gmail.com>
2020-06-21 15:59:23 +0600

    organized

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -6,8 +6,8 @@ const Card = ({name, email, id}) => {
 		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
 
 			<img alt='robot' 
-			src={`https://robohash.org/${id}$?200x200`}/>
-			<h1>{ name }</h1>
+			src={`https://robohash.org/${id}?200x200`}/>
+			<h2>{ name }</h2>
 			<p>{ email }</p>
 
 		</div>
